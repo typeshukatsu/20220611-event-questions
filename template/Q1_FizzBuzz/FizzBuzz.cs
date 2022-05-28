25930d09199d1cdbbe6fefe96467568af8fe357d
reiFujise <105709454+reiFujise@users.noreply.github.com>
2022-05-28 21:29:26 +0900

    Update FizzBuzz.cs

--- template/Q1_FizzBuzz/FizzBuzz.cs
+++ template/Q1_FizzBuzz/FizzBuzz.cs
@@ -5,7 +5,10 @@ public class FizzBuzz{
             var str = "";
 
             // ここから記述
-
+            
+			if (i % 3 == 0 && i % 5 == 0) System.Console.WriteLine("FizzBuzz");
+			else if (i % 3 == 0) System.Console.WriteLine("Fizz");
+	        else if (i % 5 == 0) System.Console.WriteLine("Buzz");
 
             // ここから記述
 
